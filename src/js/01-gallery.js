facab79932ca0946c8b09a91cef2f7f7af7a5557
jokerav <50701756+jokerav@users.noreply.github.com>
2022-02-05 17:50:05 +0200

    Пробую первое задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+createGalleryMarkup();
+
+function createGalleryMarkup() {
+  galleryItems.map(img => {
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      `<a class="gallery__item" href=${img.original}>
+  <img class="gallery__image" src=${img.preview} alt=${img.description} />
+</a>`,
+    );
+  });
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: '--animation-duration',
+});
