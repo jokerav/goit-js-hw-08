03611446d05532bf7834894c66ca9ebdec3b8735
jokerav <50701756+jokerav@users.noreply.github.com>
2022-02-06 11:31:25 +0200

    Сделал 3-е задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector("input[type='email']");
+const messageInput = document.querySelector("textarea[name='message']");
+
+if (localStorage.getItem('feedback-form-state')) {
+  const localS = localStorage.getItem('feedback-form-state');
+  const parsedState = JSON.parse(localS);
+  emailInput.value = parsedState.email;
+  messageInput.value = parsedState.message;
+}
+
+form.addEventListener('input', throttle(onInputChange, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInputChange() {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: emailInput.value,
+      message: messageInput.value,
+    }),
+  );
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const msg = { email: emailInput.value, message: messageInput.value };
+  console.log(msg);
+  event.currentTarget.reset();
+}
