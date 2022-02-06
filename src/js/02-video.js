0918f1078f7ec9d25de36e34689445c64a0488c1
jokerav <50701756+jokerav@users.noreply.github.com>
2022-02-06 08:52:01 +0200

    Сделал второе

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+if (localStorage.getItem('videoplayer-current-time')) {
+  const localTime = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(localTime);
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds.toString());
+  }, 2000),
+);
